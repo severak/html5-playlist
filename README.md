cb32cd7d25a562ecc3a1835922c225dcd9f8e8f9
severak <strajt@dalten.cz>
2021-05-24 23:18:58 +0200

    First working version.

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
 # html5-playlist
-adds missing playlist to HTML5 audio
+
+adds missing playlist to HTML5 audio element.
+
+## How?
+
+Add your audio element with first song of playlist and `data-playlist-id` attribute:
+
+```
+<audio src="trololo.mp3" controls data-playlist-id="my_awesome_playlist"></audio>
+```
+
+Add playlist iself as `li` of `a` tags:
+
+```
+<ul id="my_awesome_playlist">
+    <li><a href="trololo.mp3">trololo</a></li>
+    <li><a href="sandstorm.mp3">Sandstorm by Darude</a></li>
+</ul>
+```
+
+Include javascript file AFTER playlist:
+
+```
+<script src="html5-playlist.js"></script>
+```
+
+???
+
+Profit!
+
+
---
+++ html5-playlist.js
@@ -0,0 +1,50 @@
+(function (document, window) {
+    if (window.NodeList && !NodeList.prototype.forEach) {
+        NodeList.prototype.forEach = function (callback, thisArg) {
+            thisArg = thisArg || window;
+            for (var i = 0; i < this.length; i++) {
+                callback.call(thisArg, this[i], i, this);
+            }
+        };
+    }
+
+    document.querySelectorAll('audio').forEach(function (audio) {
+        if (audio.getAttribute('data-playlist-id') && document.getElementById(audio.getAttribute('data-playlist-id'))) {
+            var playlist = document.getElementById(audio.getAttribute('data-playlist-id')).querySelectorAll('li a');
+            var playlistTag = document.getElementById(audio.getAttribute('data-playlist-id'));
+            if (playlist.length) {
+                playlist[0].classList.add('current-track');
+                var playingSongNo = 0;
+
+                audio.addEventListener('ended', function () {
+                    // track ended, skip to next:
+                    playlist.forEach(function (elem) {
+                        elem.classList.remove('current-track');
+                    });
+                    playingSongNo++;
+                    if (playlist[playingSongNo]) {
+                        audio.src = playlist[playingSongNo].href;
+                        playlist[playingSongNo].classList.add('current-track');
+                        audio.play();
+                    }
+                });
+
+                playlistTag.addEventListener('click', function (event) {
+                    // track name clicked, play it:
+                    event.preventDefault();
+                    var ord = 0;
+                    playlist.forEach(function (elem) {
+                        elem.classList.remove('current-track');
+                        if (elem == event.target) {
+                            playingSongNo = ord;
+                            audio.src = playlist[playingSongNo].href;
+                            playlist[playingSongNo].classList.add('current-track');
+                            audio.play();
+                        }
+                        ord++;
+                    });
+                })
+            }
+        }
+    });
+})(document, window);
